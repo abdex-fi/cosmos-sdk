9f2bba381d915415d30dcbb1b16c1f8f0418b117
gbr <gbr@abdex.fi>
2025-07-20 20:53:00 +0800

    feat: Refactor LegacyDec encoding methods and remove legacy file

--- math/legacy_dec.go
+++ math/legacy_dec.go
@@ -1,6 +1,7 @@
 package math
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -15,6 +16,9 @@ import (
 type LegacyDec struct {
 	i *big.Int
 }
+
+// abdex: Dec is an alias for LegacyDec
+type Dec = LegacyDec
 
 const (
 	// LegacyPrecision number of decimal places
@@ -56,8 +60,8 @@ func init() {
 	for i := 0; i <= LegacyPrecision; i++ {
 		precisionMultipliers[i] = calcPrecisionMultiplier(int64(i))
 	}
-	// 2^256 * 10^18 -1
-	tmp := new(big.Int).Exp(big.NewInt(2), big.NewInt(256), nil)
+	// abdex: 2^256 * 10^18 -1 -> 2^512 * 10^18 -1
+	tmp := new(big.Int).Exp(big.NewInt(2), big.NewInt(512), nil)
 	tmp = new(big.Int).Sub(new(big.Int).Mul(tmp, precisionReuse), big.NewInt(1))
 	upperLimit = LegacyNewDecFromBigIntWithPrec(tmp, LegacyPrecision)
 	lowerLimit = upperLimit.Neg()
@@ -833,56 +837,119 @@ func (d LegacyDec) MarshalYAML() (interface{}, error) {
 	return d.String(), nil
 }
 
-// Marshal implements the gogo proto custom type interface.
-// func (d LegacyDec) Marshal() ([]byte, error) {
-// 	i := d.i
-// 	if i == nil {
-// 		i = new(big.Int)
-// 	}
-// 	return i.MarshalText()
-// }
+// abdex: Marshal implements the gogo proto custom type interface.
+func (d LegacyDec) Marshal() ([]byte, error) {
+	i := d.i
+	if i == nil {
+		i = new(big.Int)
+	}
 
-// MarshalTo implements the gogo proto custom type interface.
-// func (d *LegacyDec) MarshalTo(data []byte) (n int, err error) {
-// 	i := d.i
-// 	if i == nil {
-// 		i = new(big.Int)
-// 	}
+	// Use the ordered encoding instead of the default MarshalText
+	result := new(bytes.Buffer)
 
-// 	if i.Sign() == 0 {
-// 		copy(data, []byte{0x30})
-// 		return 1, nil
-// 	}
+	// Get the absolute value bytes
+	absI := new(big.Int).Abs(i)
+	rawBytes := absI.Bytes()
 
-// 	bz, err := d.Marshal()
-// 	if err != nil {
-// 		return 0, err
-// 	}
+	// Write length (fixed 1 byte, supports integers up to 255 bytes)
+	if len(rawBytes) > 255 {
+		return nil, errors.New("integer too large, exceeds 255 bytes")
+	}
 
-// 	copy(data, bz)
-// 	return len(bz), nil
-// }
+	if i.Sign() < 0 {
+		// For negative numbers: sign=0, inverted length, inverted content
+		result.WriteByte(0) // negative sign
+		// Length needs to be inverted: larger absolute values should come first (smaller in sort order)
+		result.WriteByte(255 - byte(len(rawBytes)))
+		// Content also needs to be inverted for proper ordering
+		for _, b := range rawBytes {
+			result.WriteByte(255 - b)
+		}
+	} else {
+		// For positive or zero: sign=1, normal length, normal content
+		result.WriteByte(1)                   // positive sign
+		result.WriteByte(byte(len(rawBytes))) // normal length
+		result.Write(rawBytes)
+	}
 
-// Unmarshal implements the gogo proto custom type interface.
-// func (d *LegacyDec) Unmarshal(data []byte) error {
-// 	if len(data) == 0 {
-// 		d = nil
-// 		return nil
-// 	}
+	return result.Bytes(), nil
+}
 
-// 	if d.i == nil {
-// 		d.i = new(big.Int)
-// 	}
+// abdex: MarshalTo implements the gogo proto custom type interface.
+func (d *LegacyDec) MarshalTo(data []byte) (n int, err error) {
+	i := d.i
+	if i == nil {
+		i = new(big.Int)
+	}
 
-// 	if err := d.i.UnmarshalText(data); err != nil {
-// 		return err
-// 	}
+	// For zero value, we still encode it properly using our ordered encoding
+	bz, err := d.Marshal()
+	if err != nil {
+		return 0, err
+	}
 
-// 	if !d.IsInValidRange() {
-// 		return errors.New("decimal out of range")
-// 	}
-// 	return nil
-// }
+	if len(bz) > len(data) {
+		return 0, errors.New("buffer too small")
+	}
+
+	copy(data, bz)
+	return len(bz), nil
+}
+
+// abdex: Unmarshal implements the gogo proto custom type interface.
+func (d *LegacyDec) Unmarshal(data []byte) error {
+	if len(data) == 0 {
+		d = nil
+		return nil
+	}
+
+	if d.i == nil {
+		d.i = new(big.Int)
+	}
+
+	if len(data) < 2 {
+		return errors.New("invalid encoding: too short")
+	}
+
+	// Read sign byte
+	sign := data[0]
+
+	// Read length
+	var length int
+	if sign == 0 {
+		// Negative number: length was inverted during encoding
+		length = int(255 - data[1])
+	} else {
+		// Positive number: length is normal
+		length = int(data[1])
+	}
+
+	if len(data) < 2+length {
+		return errors.New("invalid encoding: content length mismatch")
+	}
+
+	// Read content
+	content := data[2 : 2+length]
+
+	// Construct big.Int
+	if sign == 0 {
+		// Negative number: content was inverted during encoding
+		inverted := make([]byte, len(content))
+		for i, b := range content {
+			inverted[i] = 255 - b
+		}
+		d.i.SetBytes(inverted)
+		d.i.Neg(d.i)
+	} else {
+		// Positive or zero: content is normal
+		d.i.SetBytes(content)
+	}
+
+	if !d.IsInValidRange() {
+		return errors.New("decimal out of range")
+	}
+	return nil
+}
 
 // Size implements the gogo proto custom type interface.
 func (d *LegacyDec) Size() int {
